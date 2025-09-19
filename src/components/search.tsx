17f4777df313c9de147a3580bcdd9edcf265b551
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-09-19 07:47:33 -0300

    feat: implement debouncing for search input and refactor search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,36 +1,37 @@
 import { SearchIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router'
+import { useDebounce } from '../hooks/use-debounce'
 
 export function Search() {
 	const [searchParams, setSearchParams] = useSearchParams()
+	const [searchTerm, setSearchTerm] = useState(searchParams.get('search') || '')
 
-	const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault()
-		const formData = new FormData(event.currentTarget)
-		const query = formData.get('search')?.toString().trim()
+	const debouncedSearchTerm = useDebounce(searchTerm, 500)
 
+	useEffect(() => {
 		setSearchParams((state) => {
-			if (query) {
+			if (debouncedSearchTerm) {
 				state.delete('category')
-				state.set('search', query)
+				state.set('search', debouncedSearchTerm)
 			} else {
 				state.delete('search')
 			}
 
 			return state
 		})
-	}
+	}, [debouncedSearchTerm, setSearchParams])
 
 	return (
-		<form onSubmit={handleSearch} className="relative mt-4">
+		<div className="relative mt-4">
 			<SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none size-5" />
 			<input
-				defaultValue={searchParams.get('search') ?? ''}
-				name="search"
 				type="search"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
 				placeholder="Buscar..."
 				className="w-full pl-10 pr-4 py-1.5 border-1 border-gray-200 rounded-full bg-white transition-all duration-200 ease-in-out outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 placeholder:text-gray-400"
 			/>
-		</form>
+		</div>
 	)
 }
